c4358c2734f893e4863717ea3247b7ca551222a9
Worawit Boonsorn <palm454555@hotmail.com>
2021-06-18 19:24:44 +0700

    first commit

---
+++ todo/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Todo
+
+# Register your models here.
+
+class TodoAdmin(admin.ModelAdmin):
+    
+    list_display = ('title', 'desvription', 'active')
+
+
+admin.site.register(Todo)
